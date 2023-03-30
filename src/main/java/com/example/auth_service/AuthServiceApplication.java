53241d8c3c4754790b4ae4980f3034bfd53a8878
Lahiru Chalana <lahiruchalanairesh@gmail.com>
2023-03-30 11:29:05 +0530

    added important dependencies and should setup fb auth

--- src/main/java/com/example/auth_service/AuthServiceApplication.java
+++ src/main/java/com/example/auth_service/AuthServiceApplication.java
@@ -2,12 +2,15 @@ package com.example.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableEurekaClient
-@EnableSwagger2public
-class AuthServiceApplication {
+@EnableSwagger2
+public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
